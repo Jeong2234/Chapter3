cd6a85f5a03f99dc0bdd0cf9f6b72c3796b1a02e
JunHo <bagbamba13@gmail.com>
2024-04-20 01:00:31 +0900

    final  commit

--- app/src/main/java/com/example/chapter4/EditActivity.kt
+++ app/src/main/java/com/example/chapter4/EditActivity.kt
@@ -1,15 +1,71 @@
 package com.example.chapter4
 
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
+import android.content.Context
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import com.example.chapter4.databinding.ActivityEditBinding
 
+
+@Suppress("UNUSED_EXPRESSION")
 class EditActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityEditBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit)
+        binding = ActivityEditBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.bloodTypeSpinner.adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.blood_types,
+            android.R.layout.simple_list_item_1
+        )
+
+        binding.birthdateLayer.setOnClickListener {
+            val listener = OnDateSetListener { _, year, month, dayOfMonth ->
+                binding.birthdateTextView.text = "$year - ${month.inc()} - $dayOfMonth"
+            }
+            DatePickerDialog(
+                this,
+                listener,
+                2000,
+                1,
+                1
+            ).show()
+        }
+
+        binding.warningCheckBox.setOnCheckedChangeListener { buttonView, isChecked ->
+            binding.warningEditText.isVisible = isChecked
+        }
+
+        binding.warningEditText.isVisible = binding.warningCheckBox.isChecked
+
+        binding.saveButton.setOnClickListener {
+            saveDate()
+            finish()
+        }
+    }
+    private fun saveDate() {
+        with(getSharedPreferences(USER_INFORMATION, Context.MODE_PRIVATE).edit()) {
+            putString(NAME, binding.nameEditText.text.toString())
+            putString(BLOOD_TYPE, getBloodType())
+            putString(EMERGENCY_CONTACT, binding.emergencyContactEditText.text.toString())
+            putString(BIRTHDATE, binding.birthdateTextView.text.toString())
+            putString(WARING, getWarning())
+            apply()
+        }
+        Toast.makeText(this, "저장을 완료했습니다", Toast.LENGTH_LONG).show()
+    }
+    private fun getBloodType(): String {
+        val bloodAlphabet = binding.bloodTypeSpinner.selectedItem.toString()
+        val bloodSign = if (binding.bloodTypePlus.isChecked) "+" else "-"
+        return "$bloodAlphabet$bloodSign"
+    }
+    private fun getWarning(): String {
+        return if (binding.warningCheckBox.isChecked) binding.warningEditText.text.toString() else ""
     }
 }
--- app/src/main/java/com/example/chapter4/MainActivity.kt
+++ app/src/main/java/com/example/chapter4/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.chapter4
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import com.example.chapter4.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,5 +19,44 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, EditActivity::class.java)
             startActivity(intent)
         }
+        binding.deleteButton.setOnClickListener {
+            deleteDate()
+        }
+        binding.emergencyContactLayer.setOnClickListener {
+             with(Intent(Intent.ACTION_VIEW)) {
+                val phoneNumber = binding.emergencyContactValueTextView.text.toString()
+                    .replace("-", "")
+                 data = Uri.parse("tel:$phoneNumber")
+                 startActivity(this)
+            }
+        }
+    }
+    private fun getDateUiUpdate() {
+        with(getSharedPreferences(USER_INFORMATION, MODE_PRIVATE)) {
+            binding.nameValueTextView.text = getString(NAME, "미정")
+            binding.birthdateValueTextView.text = getString(BIRTHDATE, "미정")
+            binding.bloodTypeValueTextView.text = getString(BLOOD_TYPE, "미정")
+            binding.emergencyContactValueTextView.text = getString(EMERGENCY_CONTACT, "미정")
+            val waring = getString(WARING, "")
+            binding. warningTextView.isVisible = waring.isNullOrEmpty().not()
+            binding.warningValueTextView.isVisible = waring.isNullOrEmpty().not()
+            if (!waring.isNullOrEmpty()) {
+                binding.warningValueTextView.text = waring
+            }
+        }
+    }
+
+    private fun deleteDate() {
+        with(getSharedPreferences(USER_INFORMATION, MODE_PRIVATE).edit()) {
+            clear()
+            apply()
+            getDateUiUpdate()
+        }
+        Toast.makeText(this, "초기화를 완료했습니다", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onResume() {
+        getDateUiUpdate()
+        super.onResume()
     }
 }
