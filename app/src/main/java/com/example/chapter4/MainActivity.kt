4fc48a50e822e198ccd4fde1525579d88c3dfb99
JunHo <bagbamba13@gmail.com>
2024-04-19 21:04:37 +0900

    first commit

---
+++ app/src/main/java/com/example/chapter4/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.chapter4
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.chapter4.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.goInputActivityButton.setOnClickListener {
+            val intent = Intent(this, EditActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
